64c9e46a7b0cb5470322a8a4157979fcd0984cf4
mwong770 <mwong770@gmail.com>
2017-04-20 01:12:03 -0400

    adds comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,13 +37,16 @@
   $("#player1Choice").hide();
   $("#player2Choice").hide();
 
+  //updates values in Firebase
   updateChats();
   updateMessages();
   updateTurn();
-     
+
   function resetChoices() {
+      //reassigns turn to player 1
       playerTurn = "player1";
       updateTurn();
+      //removes last choice made from browser
       if (playerNumber == 1){
             $("#player1Choice").empty();
             $("#player2Choice").hide();
@@ -51,6 +54,7 @@
                 $("#player1Choice").hide();
                 $("#player2Choice").empty();
             }
+      //removes last choice from Firebase
       player2Choice = "";
       player1Choice = "";
       database.ref("Player1/").update({
@@ -63,6 +67,7 @@
       });
   }
 
+  //compares choices made, determines winner, and updates values in Firebase
   function compareChoices() {
         playerTurn = "";
         updateTurn();
@@ -142,6 +147,7 @@
       });
   }
 
+  //updates message text in Firebase
   function updateMessages() {
       database.ref("Messages/").update({
           player1Message: player1Message,
@@ -149,18 +155,21 @@
       });
   }
 
+  //updates player's turn in Firebase
   function updateTurn() {
       database.ref("Turn/").update({
           playerTurn: playerTurn
       });
   }
 
+  //updates chat text in Firebase
   function updateChats() {
       database.ref("Chats/").update({
           chat: chat
       });
   }
 
+  //sets message indicating players' turn
   function startGame() {
       player1Message = "It is your turn. Choose rock, paper, or scissors."
       player2Message = "It is player 1's turn to choose rock, paper, or scissors."
@@ -169,6 +178,7 @@
       updateMessages();
   }
 
+  //sets player1 div border to green and returns player 2 div to blue 
   function startTurn() {
       $("#player1ScoreDiv").css("border", "2px solid green");
       $("#player2ScoreDiv").css("border", "2px solid blue");
@@ -179,13 +189,14 @@ $(document).ready(function(){
     $("#submitName").on('click', function(event) {
         //prevents the submit button from trying to submit a form when clicked
         event.preventDefault();
+        //gets player values from Firebase
         database.ref("Player1/").once('value', function(snapshot){
             player1  = snapshot.val();
         });
         database.ref("Player2/").once('value', function(snapshot){
             player2  = snapshot.val();
         });
-
+        //if no player1, assigns name inputted to player1 and sets initial player 1 values
         if (!player1 && playerNumber == 0) {
                 player1Name = $("#name").val().trim();
                 if (!player1Name == "") {
@@ -203,6 +214,7 @@ $(document).ready(function(){
                     $("#player1Choice").show();
                     $("#name").val("");
                 }
+        //if no player2, assigns name inputted to player1 and sets initial player 1 values
         }   else if (!player2 && playerNumber == 0) {
                 player2Name = $("#name").val().trim();
                 if (!player2Name == "") {
@@ -224,58 +236,25 @@ $(document).ready(function(){
                     });
                     $("#name").val("");
                 }
+                //if already have player1 and player2, displays message
+                //specified if playerNumber == 0 here to only display message if not already a player
             }   else if (playerNumber == 0) {
                     $("#message").html("We already have 2 players. Please wait your turn.").show();
                 }
+        //displays player names based on player number
         if (playerNumber == 1){
             $('#player1Name').html("Hello, " + player1Name);
         }   else if (playerNumber == 2){
                 $("#player2Name").html("Hello, " + player2Name);
             }
+        //calls startGame function if both players assigned
         if (!player1Name == "" && !player2Name == "") {
             startGame();
         }
     });
 
-    //when Firebase values change, 
-    //updates variables with Firebase values, compares values, and displays scores
-    database.ref("Player1/").on('value', function(snapshot){
-        player1Values = snapshot.val();
-        if (!player1Values) {
-            $('#player1Name').html("Waiting for player 1");
-        }   else {
-                player1Name = player1Values.player1Name;
-                $("#player1Name").html("Player 1 is " + player1Name);
-                player1Choice = player1Values.player1Choice;
-                player1Wins = player1Values.player1Wins;
-                player1Losses = player1Values.player1Losses;
-                player1Ties = player1Values.player1Ties;
-                $('#player1Ties').html(player1Ties);
-                $('#player1Wins').html(player1Wins);
-                $('#player1Losses').html(player1Losses);
-                $("#player1Choice").html(player1Choice);
-            }
-    });
-
-    database.ref("Player2/").on('value', function(snapshot){
-        player2Values = snapshot.val();
-        if (!player2Values) {
-            $('#player2Name').html("Waiting for player 2");
-        }   else {
-                player2Name = player2Values.player2Name;
-                $("#player2Name").html("Player 2 is " + player2Name);
-                player2Choice = player2Values.player2Choice;
-                player2Wins = player2Values.player2Wins;
-                player2Losses = player2Values.player2Losses;
-                player2Ties = player2Values.player2Ties;
-                $('#player2Ties').html(player2Ties);
-                $('#player2Wins').html(player2Wins);
-                $('#player2Losses').html(player2Losses);
-                $("#player2Choice").html(player2Choice);
-            }
-    });
-
     $('.playerOptions').on('click', function(){
+        //assigns choice to player1 if no choices made
         if (player1Choice == "" && player2Choice == "") {
             if (playerNumber == 1) {
                 player1Choice = $(this).attr('value');
@@ -288,7 +267,9 @@ $(document).ready(function(){
                 playerTurn = "player2";
                 updateTurn();
                 updateMessages();
-            } 
+            }
+            //assigns choice to player2 if player 1 choice made and not player 2 choice
+            //calls function to compare choices
         }   else if (!player1Choice == "" && player2Choice == "") {
                 $("#player1Choice").show();
                 $("#player2Choice").show();
@@ -303,6 +284,7 @@ $(document).ready(function(){
             }   
     });
 
+    //updates in Firebase chats inputted in larger browsers
     $('.submitChats').on('click', function(event){
         event.preventDefault();
         chat = $("#chat").val().trim();
@@ -321,6 +303,7 @@ $(document).ready(function(){
         }
     });
 
+    //updates in Firebase chats inputted in smaller browsers
     $('.submitChats2').on('click', function(event){
         event.preventDefault();
         chat = $("#chat2").val().trim();
@@ -339,6 +322,46 @@ $(document).ready(function(){
         }
     });
 
+    //updates browser when player 1 values stored in Firebase change
+    database.ref("Player1/").on('value', function(snapshot){
+        player1Values = snapshot.val();
+        if (!player1Values) {
+            $('#player1Name').html("Waiting for player 1");
+        }   else {
+                player1Name = player1Values.player1Name;
+                $("#player1Name").html("Player 1 is " + player1Name);
+                player1Choice = player1Values.player1Choice;
+                player1Wins = player1Values.player1Wins;
+                player1Losses = player1Values.player1Losses;
+                player1Ties = player1Values.player1Ties;
+                $('#player1Ties').html(player1Ties);
+                $('#player1Wins').html(player1Wins);
+                $('#player1Losses').html(player1Losses);
+                $("#player1Choice").html(player1Choice);
+            }
+    });
+
+    //updates browser when player 2 values stored in Firebase change
+    database.ref("Player2/").on('value', function(snapshot){
+        player2Values = snapshot.val();
+        if (!player2Values) {
+            $('#player2Name').html("Waiting for player 2");
+        }   else {
+                player2Name = player2Values.player2Name;
+                $("#player2Name").html("Player 2 is " + player2Name);
+                player2Choice = player2Values.player2Choice;
+                player2Wins = player2Values.player2Wins;
+                player2Losses = player2Values.player2Losses;
+                player2Ties = player2Values.player2Ties;
+                $('#player2Ties').html(player2Ties);
+                $('#player2Wins').html(player2Wins);
+                $('#player2Losses').html(player2Losses);
+                $("#player2Choice").html(player2Choice);
+            }
+    });
+
+    //updates browser when chat text stored in Firebase changes
+    //adds automatic scroll to chat display div so always see latest chat
     database.ref("Chats/").on('value', function(snapshot){
         chat = snapshot.val().chat + "<br/>";
         $(".displayChats").append(chat);
@@ -346,6 +369,7 @@ $(document).ready(function(){
         element.scrollTop = element.scrollHeight;
     });
 
+    //updates browser when message text stored in Firebase changes
     database.ref("Messages/").on('value', function(snapshot){
         player1Message = snapshot.val().player1Message;
         player2Message = snapshot.val().player2Message;
@@ -353,6 +377,7 @@ $(document).ready(function(){
         $("#player2Message").html(player2Message);
     });
 
+    //changes border of player div based on players' turn (as stored in Firebase) 
     database.ref("Turn/").on('value', function(snapshot){
         playerTurn = snapshot.val().playerTurn;
         if (playerTurn == "player1") {
