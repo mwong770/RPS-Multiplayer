0476b47e19b4b66f3165ff8d25288fad14d8e4a5
mwong770 <mwong770@gmail.com>
2017-08-02 23:07:37 -0400

    cleans code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,10 +50,10 @@
       playerTurn = "player1";
       updateTurn();
       //removes last choice made from browser
-      if (playerNumber == 1){
+      if (playerNumber === 1){
             $(".player1ChoiceImg").attr("src", "").attr("id", "");
             $(".player2ChoiceImg").attr("src", "").attr("id", "").hide();
-        }   else if (playerNumber == 2){
+        }   else if (playerNumber === 2){
                 $(".player1ChoiceImg").attr("src", "").attr("id", "").hide();
                 $(".player2ChoiceImg").attr("src", "").attr("id", "");
             }
@@ -74,7 +74,7 @@
   function compareChoices() {
         playerTurn = "";
         updateTurn();
-        if(player1Choice == player2Choice) {
+        if (player1Choice === player2Choice) {
             player1Message = "It's a tie. Choose rock, paper, or scissors to play again."
             player2Message = "It's a tie. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
@@ -88,32 +88,32 @@
                 player2Ties: player2Ties,
                 gameOver: true
             });
-        } else if (player1Choice == "rock" && player2Choice == "paper") {
+        } else if (player1Choice === "rock" && player2Choice === "paper") {
             player1Message = "Player 2 wins. Paper beats rock. Choose rock, paper, or scissors to play again."
             player2Message = "You win! Paper beats rock. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
             player2Win();
-        } else if(player1Choice == "rock" && player2Choice == "scissors"){
+        } else if (player1Choice === "rock" && player2Choice === "scissors"){
             player1Message = "You win! Rock beats scissors. Choose rock, paper, or scissors to play again."
             player2Message = "Player 1 wins. Rock beats scissors. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
             player1Win();
-        } else if(player1Choice == "scissors" && player2Choice == "paper"){
+        } else if (player1Choice === "scissors" && player2Choice === "paper"){
             player1Message = "You win! Scissors beats paper. Choose rock, paper, or scissors to play again."
             player2Message = "Player 1 wins. Scissors beats paper. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
             player1Win();
-        } else if(player1Choice == "scissors" && player2Choice == "rock") {
+        } else if (player1Choice === "scissors" && player2Choice === "rock") {
             player1Message = "Player 2 wins. Rock beats scissors. Choose rock, paper, or scissors to play again."
             player2Message = "You win! Rock beats scissors. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
             player2Win();
-        } else if (player1Choice == "paper" && player2Choice == "scissors") {
+        } else if (player1Choice === "paper" && player2Choice === "scissors") {
             player1Message = "Player 2 wins. Scissors beats paper. Choose rock, paper, or scissors to play again."
             player2Message = "You win! Scissors beats paper. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
             player2Win();
-        } else if(player1Choice == "paper" && player2Choice == "rock"){
+        } else if (player1Choice === "paper" && player2Choice === "rock"){
             player1Message = "You win! Paper beats rock. Choose rock, paper, or scissors to play again."
             player2Message = "Player 1 wins. Paper beats rock. Wait for player 1 to choose rock, paper, or scissors to play again."
             updateMessages();
@@ -189,17 +189,17 @@
 
   //plays sounds to be used when options clicked or user wins
   function sound(soundFile) {
-    soundFile.loop = false;
-        soundFile.play();
+      soundFile.loop = false;
+      soundFile.play();
   }
 
   //when close or refresh browser, removes leaving player's data
   window.onbeforeunload = function() {
-      if (playerNumber == 1) {
+      if (playerNumber === 1) {
           var player1Ref = database.ref("Player1/");
           player1Ref.remove();
       }   
-          else if (playerNumber == 2) {
+          else if (playerNumber === 2) {
               var player2Ref = database.ref("Player2/");
               player2Ref.remove();
           }
@@ -207,20 +207,20 @@
 
 $(document).ready(function(){
 
-    $("#submitName").on('click', function(event) {
+    $("#submitName").on("click", function(event) {
         //prevents the submit button from trying to submit a form when clicked
         event.preventDefault();
         //gets player values from Firebase
-        database.ref("Player1/").once('value', function(snapshot){
+        database.ref("Player1/").once("value", function(snapshot){
             player1  = snapshot.val();
         });
-        database.ref("Player2/").once('value', function(snapshot){
+        database.ref("Player2/").once("value", function(snapshot){
             player2  = snapshot.val();
         });
         //if no player1, assigns name inputted to player1 and sets initial player 1 values
-        if (!player1 && playerNumber == 0) {
+        if (!player1 && playerNumber === 0) {
                 player1Name = $("#name").val().trim();
-                if (!player1Name == "") {
+                if (!player1Name === "") {
                     playerNumber = 1;
                     database.ref("Player1/").update({
                         player1Name: player1Name,
@@ -236,9 +236,9 @@ $(document).ready(function(){
                     $("#name").val("");
                 }
         //if no player2, assigns name inputted to player1 and sets initial player 1 values
-        }   else if (!player2 && playerNumber == 0) {
+        }   else if (!player2 && playerNumber === 0) {
                 player2Name = $("#name").val().trim();
-                if (!player2Name == "") {
+                if (!player2Name === "") {
                     playerNumber = 2;
                     player1Message = "Player 2 has just joined the game."
                     database.ref("Messages/").update({
@@ -259,34 +259,34 @@ $(document).ready(function(){
                 }
                 //if already have player1 and player2, displays message
                 //specified if playerNumber == 0 here to only display message if not already a player
-            }   else if (playerNumber == 0) {
+            }   else if (playerNumber === 0) {
                     $("#message").html("We already have 2 players. Please wait your turn.").show();
                 }
         //displays player names based on player number
-        if (playerNumber == 1){
-            $('#player1Name').html("Hello, " + player1Name);
-        }   else if (playerNumber == 2){
+        if (playerNumber === 1){
+            $("#player1Name").html("Hello, " + player1Name);
+        }   else if (playerNumber === 2){
                 $("#player2Name").html("Hello, " + player2Name);
-                $('#player1Name').html("Player 1 is " + player1Name);
+                $("#player1Name").html("Player 1 is " + player1Name);
             }
         //calls startGame function if both players assigned
-        if (!player1Name == "" && !player2Name == "") {
+        if (!player1Name === "" && !player2Name === "") {
             startGame();
         }
     });
 
-    $('.playerOptions').on('click', function(){
+    $(".playerOptions").on("click", function(){
         //assigns choice to player1 if no choices made
-        if (player1Choice == "" && player2Choice == "") {
-            if (playerNumber == 1) {
-                player1Choice = $(this).attr('value');
-                if (player1Choice == "scissors") {
+        if (player1Choice === "" && player2Choice === "") {
+            if (playerNumber === 1) {
+                player1Choice = $(this).attr("value");
+                if (player1Choice === "scissors") {
                     $(".player1ChoiceImg").attr("src", "assets/images/scissors.png").attr("id", "scissorsImg");
                     sound(scissorsSound);
-                }  else if (player1Choice == "rock") {
+                }  else if (player1Choice === "rock") {
                       $(".player1ChoiceImg").attr("src", "assets/images/rock.png").attr("id", "rockImg");
                       sound(rockSound);
-                   }  else if (player1Choice == "paper") {
+                   }  else if (player1Choice === "paper") {
                           $(".player1ChoiceImg").attr("src", "assets/images/paper.png").attr("id", "paperImg");
                           sound(paperSound);
                       }
@@ -301,18 +301,18 @@ $(document).ready(function(){
             }
             //assigns choice to player2 if player 1 choice made and not player 2 choice
             //calls function to compare choices
-        }   else if (!player1Choice == "" && player2Choice == "") {
+        }   else if (!player1Choice === "" && player2Choice === "") {
                 $(".player1ChoiceImg").show();
                 $(".player2ChoiceImg").show();
-                if (playerNumber == 2) {
-                    player2Choice = $(this).attr('value');
-                    if (player2Choice == "scissors") {
+                if (playerNumber === 2) {
+                    player2Choice = $(this).attr("value");
+                    if (player2Choice === "scissors") {
                         $(".player2ChoiceImg").attr("src", "assets/images/scissors.png").attr("id", "scissorsImg");
                         sound(scissorsSound);
-                    }  else if (player2Choice == "rock") {
+                    }  else if (player2Choice === "rock") {
                           $(".player2ChoiceImg").attr("src", "assets/images/rock.png").attr("id", "rockImg");
                           sound(rockSound);
-                       }  else if (player2Choice == "paper") {
+                       }  else if (player2Choice === "paper") {
                               $(".player2ChoiceImg").attr("src", "assets/images/paper.png").attr("id", "paperImg");
                               sound(paperSound);
                           }
@@ -325,16 +325,16 @@ $(document).ready(function(){
     });
 
     //updates in Firebase chats inputted in larger browsers
-    $('.submitChats').on('click', function(event){
+    $(".submitChats").on("click", function(event){
         event.preventDefault();
         chat = $("#chat").val().trim();
-        if (!chat == "") {
-            if (!playerNumber == 0) {
+        if (!chat === "") {
+            if (!playerNumber === 0) {
                 var player1Uppercase = player1Name.toUpperCase();
                 var player2Uppercase = player2Name.toUpperCase();
-                if (playerNumber == 1) {
+                if (playerNumber === 1) {
                     chat = player1Uppercase + ": " + chat;
-                }   else if (playerNumber == 2) {
+                }   else if (playerNumber === 2) {
                         chat = player2Uppercase + ": " + chat;
                     }
                 updateChats();
@@ -344,16 +344,16 @@ $(document).ready(function(){
     });
 
     //updates in Firebase chats inputted in smaller browsers
-    $('.submitChats2').on('click', function(event){
+    $(".submitChats2").on("click", function(event){
         event.preventDefault();
         chat = $("#chat2").val().trim();
-        if (!chat == "") {
-            if (!playerNumber == 0) {
+        if (!chat === "") {
+            if (!playerNumber === 0) {
                 var player1Uppercase = player1Name.toUpperCase();
                 var player2Uppercase = player2Name.toUpperCase();
-                if (playerNumber == 1) {
+                if (playerNumber === 1) {
                     chat = player1Uppercase + ": " + chat;
-                }   else if (playerNumber == 2) {
+                }   else if (playerNumber === 2) {
                         chat = player2Uppercase + ": " + chat;
                     }
                 updateChats();
@@ -363,58 +363,58 @@ $(document).ready(function(){
     });
 
     //updates browser when player 1 values stored in Firebase change
-    database.ref("Player1/").on('value', function(snapshot){
+    database.ref("Player1/").on("value", function(snapshot) {
         player1Values = snapshot.val();
         if (!player1Values) {
-            $('#player1Name').html("Waiting for player 1");
+            $("#player1Name").html("Waiting for player 1");
         }   else {
                 player1Name = player1Values.player1Name;
-                if (playerNumber == 2) {
+                if (playerNumber === 2) {
                     $("#player1Name").html("Player 1 is " + player1Name);
                 }
                 player1Choice = player1Values.player1Choice;
                 player1Wins = player1Values.player1Wins;
                 player1Losses = player1Values.player1Losses;
                 player1Ties = player1Values.player1Ties;
-                $('#player1Ties').html(player1Ties);
-                $('#player1Wins').html(player1Wins);
-                $('#player1Losses').html(player1Losses);
-                if (player1Choice == "scissors") {
+                $("#player1Ties").html(player1Ties);
+                $("#player1Wins").html(player1Wins);
+                $("#player1Losses").html(player1Losses);
+                if (player1Choice === "scissors") {
                     $(".player1ChoiceImg").attr("src", "assets/images/scissors.png").attr("id", "scissorsImg");
-                }  else if (player1Choice == "rock") {
+                }  else if (player1Choice === "rock") {
                       $(".player1ChoiceImg").attr("src", "assets/images/rock.png").attr("id", "rockImg");
-                   }  else if (player1Choice == "paper") {
+                   }  else if (player1Choice === "paper") {
                           $(".player1ChoiceImg").attr("src", "assets/images/paper.png").attr("id", "paperImg");
-                      }   else if (player1Choice == "") {
+                      }   else if (player1Choice === "") {
                               $(".player1ChoiceImg").attr("src", "").attr("id", "");
                           }
             }
     });
 
     //updates browser when player 2 values stored in Firebase change
-    database.ref("Player2/").on('value', function(snapshot){
+    database.ref("Player2/").on("value", function(snapshot){
         player2Values = snapshot.val();
         if (!player2Values) {
-            $('#player2Name').html("Waiting for player 2");
+            $("#player2Name").html("Waiting for player 2");
         }   else {
                 player2Name = player2Values.player2Name;
-                if (playerNumber == 1) {
+                if (playerNumber === 1) {
                     $("#player2Name").html("Player 2 is " + player2Name);
                 }
                 player2Choice = player2Values.player2Choice;
                 player2Wins = player2Values.player2Wins;
                 player2Losses = player2Values.player2Losses;
                 player2Ties = player2Values.player2Ties;
-                $('#player2Ties').html(player2Ties);
-                $('#player2Wins').html(player2Wins);
-                $('#player2Losses').html(player2Losses);
-                if (player2Choice == "scissors") {
+                $("#player2Ties").html(player2Ties);
+                $("#player2Wins").html(player2Wins);
+                $("#player2Losses").html(player2Losses);
+                if (player2Choice === "scissors") {
                         $(".player2ChoiceImg").attr("src", "assets/images/scissors.png").attr("id", "scissorsImg");
-                    }  else if (player2Choice == "rock") {
+                    }  else if (player2Choice === "rock") {
                           $(".player2ChoiceImg").attr("src", "assets/images/rock.png").attr("id", "rockImg");
-                       }  else if (player2Choice == "paper") {
+                       }  else if (player2Choice === "paper") {
                               $(".player2ChoiceImg").attr("src", "assets/images/paper.png").attr("id", "paperImg");
-                          }   else if (player2Choice == "") {
+                          }   else if (player2Choice === "") {
                                   $(".player2ChoiceImg").attr("src", "").attr("id", "");
                               }
             }
@@ -422,7 +422,7 @@ $(document).ready(function(){
 
     //updates browser when chat text stored in Firebase changes
     //adds automatic scroll to chat display div so always see latest chat
-    database.ref("Chats/").on('value', function(snapshot){
+    database.ref("Chats/").on("value", function(snapshot){
         chat = snapshot.val().chat + "<br/>";
         $(".displayChats").append(chat);
         element = document.getElementById("displayChats2");
@@ -430,7 +430,7 @@ $(document).ready(function(){
     });
 
     //updates browser when message text stored in Firebase changes
-    database.ref("Messages/").on('value', function(snapshot){
+    database.ref("Messages/").on("value", function(snapshot){
         player1Message = snapshot.val().player1Message;
         player2Message = snapshot.val().player2Message;
         $("#player1Message").html(player1Message);
@@ -438,12 +438,12 @@ $(document).ready(function(){
     });
 
     //changes border of player div based on players' turn (as stored in Firebase) 
-    database.ref("Turn/").on('value', function(snapshot){
+    database.ref("Turn/").on("value", function(snapshot) {
         playerTurn = snapshot.val().playerTurn;
-        if (playerTurn == "player1") {
+        if (playerTurn === "player1") {
               $("#player1ScoreDiv").css("border", "5px solid #B540B5");
               $("#player2ScoreDiv").css("border", "2px solid blue");
-        }  else if (playerTurn == "player2") {
+        }  else if (playerTurn === "player2") {
               $("#player2ScoreDiv").css("border", "5px solid #B540B5");
               $("#player1ScoreDiv").css("border", "2px solid blue");
            }  else {
